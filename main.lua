0b821a6abf58ec4073c34c62b2aebdf850906a30
darkusagi99 <darkusagi99@gmail.com>
2022-08-10 20:19:30 +0200

    Project initialisation

---
+++ main.lua
@@ -0,0 +1,23 @@
+function love.load()
+
+
+end
+
+function love.draw()	
+	
+end
+
+function love.update(dt)
+	
+end
+
+
+-- Fonction de détection des touches
+function love.keypressed(key)
+
+	-- ECHAP => Fermeture programme
+	if key == 'escape' then
+		love.event.quit()
+	end
+	
+end
